b242b0f4195d876e1bfcea3803c44ae1af07b132
kimjaeyeol <godbasic97@gmail.com>
2022-02-21 09:25:31 +0900

    Add Home template

--- src/main/java/webflux/ServerController.java
+++ src/main/java/webflux/ServerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+/* @RestController는 화면 구성을 위한 템플릿을 사용하는 대신에 결과 데이터를 직렬화하고 HTTP 응답 본문에 직접 써서 반환하는 REST 컨트롤러임을 나타낸다 */
 @RestController
 public class ServerController {
   private final KitchenService kitchen;
@@ -16,5 +17,10 @@ public class ServerController {
   @GetMapping(value = "/server", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   Flux<Dish> serveDishes() {
     return this.kitchen.getDishes();
+  }
+
+  @GetMapping(value = "/served-dishes", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  Flux<Dish> deliverDishes() {
+    return this.kitchen.getDishes().map(dish -> Dish.deliver(dish));
   }
 }
